93d3ac7c610702e169bd4a9989ecfc66ce2312ef
Miguel Muñiz <73082309+MiguelMunizJR@users.noreply.github.com>
2023-02-21 03:47:41 -0600

    add swagger docs

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const clientsRouter = require("./src/clients/clients.router");
 const authRouter = require("./src/auth/auth.router");
 //* import init models
 const initModels = require("./src/models/initModels");
+//* import swagger.JSON
+const swaggerUI = require("swagger-ui-express");
+const swaggerDoc = require("./swagger.json");
 
 const app = express();
 
@@ -32,6 +35,9 @@ app.use("/api/v1/users", usersRouter);
 app.use("/api/v1/tasks", tasksRouter);
 app.use("/api/v1/clients", clientsRouter);
 app.use("/api/v1/auth", authRouter);
+
+//* SWAGGER
+app.use("/api/docs", swaggerUI.serve, swaggerUI.setup(swaggerDoc));
 
 app.listen(PORT, () => {
   console.log(`🌐 Server online at port: ${PORT}`);
